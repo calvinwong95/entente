82ff4686f05e66b2101d8557b46c16a93d86df84
calvinwong95 <calvinwongch95@gmail.com>
2022-08-03 15:37:38 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+
+import LoginPage from "./views/LoginPage";
+
+function App() {
+  return (
+    <Box className="App">
+      <LoginPage />
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/views/LoginPage/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const LoginPage = () => {
+  return <Box>LoginPage</Box>;
+};
+
+export default LoginPage;
