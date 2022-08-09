a2d591e330dae40f48b53b8f358f8864f16fbbd2
calvinwong95 <calvinwongch95@gmail.com>
2022-08-09 21:12:54 +0800

    wip

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "../views/LoginPage";
 import MyPost from "../views/MyPost";
 import Notification from "../views/Notification";
