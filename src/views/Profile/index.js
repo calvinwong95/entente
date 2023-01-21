aec0f8c5187ca70f6983647a22a89ae802ab6ffa
Calvin Wong <calvinwongch95@gmail.com>
2023-01-21 10:11:40 +0800

    wip: created edit usestates

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -10,8 +10,15 @@ import { AccountCircle } from "@mui/icons-material";
 
 const Profile = () => {
   const classes = UseStyles();
-  const [isEdit, setIsEdit] = useState(false);
+  const [isEditProfile, setIsEditProfile] = useState(false);
+  const [isEditPost, setIsEditPost] = useState(false);
   const [openModal, setOpenModal] = useState(false);
+  const handleEditProfile = () => {
+    setIsEditProfile(!isEditProfile);
+  };
+  const handleEditPost = () => {
+    setIsEditPost(!isEditPost);
+  };
   return (
     <Dashboard>
       <Box sx={{ width: "100%", height: "100%" }}>
@@ -31,42 +38,122 @@ const Profile = () => {
               backgroundColor: "#FFFFFF",
               borderRadius: 2.5,
               padding: 2,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "space-between",
             }}
           >
-            <Typography variant="h3" sx={{ mb: 2 }}>
-              Profile Settings
-            </Typography>
-            <Box sx={{ width: "100%", overflowY: "auto" }}>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
+            <Typography variant="h3">Profile Settings</Typography>
+            {isEditProfile ? (
+              <>
                 <Box
                   sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
+                    width: "100%",
+                    height: "400px",
+                    overflowY: "auto",
+                    px: 2,
+                    "&::-webkit-scrollbar": {
+                      width: "5px",
+                    },
+                    "&::-webkit-scrollbar-track": {
+                      background: "#DCF2FF",
+                    },
+                    "&::-webkit-scrollbar-thumb": {
+                      background: "#1D388F61",
+                      borderRadius: 2,
+                    },
                   }}
                 >
-                  <AccountCircle color="white" sx={{ fontSize: 100 }} />
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <AccountCircle color="white" sx={{ fontSize: 100 }} />
+                    </Box>
+                  </Box>
+                  <TenTextField label="Username" disabled></TenTextField>
+                  <TenTextField label="Password" disabled></TenTextField>
+                  <TenDropdown label="Category" disabled>
+                    <MenuItem>One</MenuItem>
+                    <MenuItem>Two</MenuItem>
+                    <MenuItem>Three</MenuItem>
+                  </TenDropdown>
+                  <TenTextField label="Email Addresss" disabled></TenTextField>
+                  <TenTextField label="Phone Number" disabled></TenTextField>
                 </Box>
-              </Box>
-              <TenTextField label="Username" disabled></TenTextField>
-              <TenTextField label="Password" disabled></TenTextField>
-              <TenDropdown label="Category" disabled>
-                <MenuItem>One</MenuItem>
-                <MenuItem>Two</MenuItem>
-                <MenuItem>Three</MenuItem>
-              </TenDropdown>
-              <TenTextField label="Email Addresss" disabled></TenTextField>
-              <TenTextField label="Phone Number" disabled></TenTextField>
+              </>
+            ) : (
+              <>
+                {" "}
+                <Box
+                  sx={{
+                    width: "100%",
+                    height: "400px",
+                    overflowY: "auto",
+                    px: 2,
+                    "&::-webkit-scrollbar": {
+                      width: "5px",
+                    },
+                    "&::-webkit-scrollbar-track": {
+                      background: "#DCF2FF",
+                    },
+                    "&::-webkit-scrollbar-thumb": {
+                      background: "#1D388F61",
+                      borderRadius: 2,
+                    },
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <AccountCircle color="white" sx={{ fontSize: 100 }} />
+                    </Box>
+                  </Box>
+                  <TenTextField label="Username" disabled></TenTextField>
+                  <TenTextField label="Password" disabled></TenTextField>
+                  <TenDropdown label="Category" disabled>
+                    <MenuItem>One</MenuItem>
+                    <MenuItem>Two</MenuItem>
+                    <MenuItem>Three</MenuItem>
+                  </TenDropdown>
+                  <TenTextField label="Email Addresss" disabled></TenTextField>
+                  <TenTextField label="Phone Number" disabled></TenTextField>
+                </Box>
+              </>
+            )}
+
+            <Box sx={{ width: "100%" }}>
+              <TenButton variant="contained" color="third-01">
+                <Typography
+                  className={classes.whiteFont}
+                  onClick={handleEditProfile}
+                >
+                  Edit
+                </Typography>
+              </TenButton>
             </Box>
-            {/* <Box sx={{ width: "100%", backgroundColor: "red" }}>
-              <TenButton variant="contained">Edit</TenButton>
-            </Box> */}
           </Box>
           <Box
             sx={{
